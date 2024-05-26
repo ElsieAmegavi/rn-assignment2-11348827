b457bc5b362876ab3310c73975ee3a59a2d67f91
elsieroyalty <139142562+ElsieAmegavi@users.noreply.github.com>
2024-05-26 22:36:14 +0000

    background color changed to purple

--- MyApp/App.js
+++ MyApp/App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D08CF7',
     alignItems: 'center',
     justifyContent: 'center',
   },
